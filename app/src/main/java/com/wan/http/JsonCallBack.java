e5a65f5553e5a7135137253cb7d04b04c188a4fe
万友志 <906532015@qq.com>
2017-11-15 16:58:17 +0800

    更细提示

--- app/src/main/java/com/wan/http/JsonCallBack.java
+++ app/src/main/java/com/wan/http/JsonCallBack.java
@@ -25,18 +25,23 @@ public abstract class JsonCallBack<T> implements Callback {
 
     @Override
     public void onResponse(final Call call, final Response response) throws IOException {
+        String msg = "获取服务器返回失败";
         if (response.isSuccessful()) {
             ResponseBody body = response.body();
             String bodyString = body == null ? "" : body.string().trim();
-            try {
-                Object a = bodyString.startsWith("{") || bodyString.startsWith("[") ? new Gson().fromJson(bodyString, mType) : bodyString;
-                onSuccess((T) a);
-            } catch (Exception e) {
-                onFailure(call, new IOException("Json转换失败"));
+            if (bodyString.startsWith("{") || bodyString.startsWith("[")) {
+                try {
+                    Object a = new Gson().fromJson(bodyString, mType);
+                    onSuccess((T) a);
+                    return;
+                } catch (Exception e) {
+                    msg = "Json转换失败";
+                }
+            } else {
+                msg = "返回格式非json";
             }
-        } else {
-            onFailure(call, new IOException("网络开小差了，请检查网络！"));
         }
+        onFailure(call, new IOException(msg));
     }
 
     public abstract void onSuccess(T obj);
